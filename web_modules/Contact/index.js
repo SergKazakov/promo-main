9e0854ef20a266745a83e54ec2f9db5b8b2791bc
SergKazakov <sergeykazakoff@yandex.ru>
2016-07-29 17:15:18 +0300

    Change Contact component

--- web_modules/Contact/index.js
+++ web_modules/Contact/index.js
@@ -8,6 +8,38 @@ const Contact = () => (
       subtitle={ `Nullam sit amet odio eu est aliquet euismod a a urna.
                   Proin eu urna suscipit, dictum quam nec.` }
     />
+    <div className={ styles.contact }>
+      <div className={ styles.aside }>
+        <h3 className={ styles.title }>
+          { 'OUR ADDRESS' }
+        </h3>
+        <p className={ styles.text }>
+          { 'House #13, Streat road, Sydney 2310 Australia' }
+        </p>
+        <h3 className={ styles.title }>
+          { 'CALL US' }
+        </h3>
+        <p className={ styles.text }>
+          <a className={ styles.textLink } href={ 'tel:+ 880 168 109 1425' }>
+            { '+ 880 168 109 1425' }
+          </a>
+        </p>
+        <p className={ styles.text }>
+          <a className={ styles.textLink } href={ 'tel:+ 0216809142' }>
+            { '+ 0216809142' }
+          </a>
+        </p>
+        <h3 className={ styles.title }>
+          { 'EMAIL US' }
+        </h3>
+        <p className={ styles.text }>
+          <a className={ styles.textLink } href={ 'mailto:contact@email.com' }>
+            { 'contact@email.com' }
+          </a>
+        </p>
+      </div>
+      <form className={ styles.form }></form>
+    </div>
   </section>
 )
 
