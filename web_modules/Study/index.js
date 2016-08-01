c9bf44969cf714b78cf1e8dbbc46083a1b9164cd
SergKazakov <sergeykazakoff@yandex.ru>
2016-08-01 10:55:35 +0300

    Add study slider

--- web_modules/Study/index.js
+++ web_modules/Study/index.js
@@ -1,42 +1,56 @@
 import Title from "../Title"
 import Button from "../Button"
 import Svg from "react-svg-inline"
+import Slider from "react-slick"
 import styles from "./index.css"
+import studies from "./studies.json"
 
-const Study = () => (
-  <section className={ styles.wrapper }>
-    <Title
-      title={ 'CASE STUDY' }
-      subtitle={
-        "A brief story about how this process works, keep an eye till the end."
-      }
-    />
-    <div className={ styles.inner }>
-      <div className={ styles.list }>
-        <Svg
-          className={ styles.listIcon }
-          svg={ require("../Features/lamp.svg") }
-          width="3rem"
-          height="auto"
-        />
-        <h3 className={ styles.listTitle }>
-          {
-            'aCCUMULATE CREATIVE IDEAS'
-          }
-        </h3>
-        <p className={ styles.listText }>
-          {
-            `Lorem Ipsum is simply dummy text of the printing and typesetting
-            let. Lorem Ipsum has been the industry. Lorem Ipsum is simply dummy
-            text of the printing and typesetting let. Lorem Ipsum has been the
-            industry Printing and typelorem Ipsum has been the setting let.`
-          }
-        </p>
-        <Button to={ '/' }>{ 'Read more' }</Button>
+const Study = () => {
+  const settings = {
+    arrows: false,
+    slideToShow: 1,
+    slideToScroll: 1,
+    dots: true,
+    autoplay: true,
+  }
+
+  return (
+    <section className={ styles.wrapper }>
+      <Title
+        title={ 'CASE STUDY' }
+        subtitle={
+          `A brief story about how this process works,
+          keep an eye till the end.`
+        }
+      />
+      <div className={ styles.inner }>
+        <div className={ styles.list }>
+          <Slider { ...settings }>
+            {
+              studies.map(({ icon, title, text }, index) => (
+                <div key={ index }>
+                  <Svg
+                    className={ styles.listIcon }
+                    svg={ require(`../Features/${icon}.svg`) }
+                    width="3rem"
+                    height="auto"
+                  />
+                  <h3 className={ styles.listTitle }>{ title }</h3>
+                  <p className={ styles.listText }>{ text }</p>
+                  <footer className={ styles.listFooter }>
+                    <Button className={ styles.listBtn } to={ '/' }>
+                      { 'Read more' }
+                    </Button>
+                  </footer>
+                </div>
+              ))
+            }
+          </Slider>
+        </div>
+        <img className={ styles.img } src={ require("./study.jpg") } />
       </div>
-      <img className={ styles.img } src={ require("./study.jpg") } />
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default Study
