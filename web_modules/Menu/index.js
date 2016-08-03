11fa4959c032cd6f18808319e3435cd3d85d5b67
SergKazakov <sergeykazakoff@yandex.ru>
2016-08-03 13:01:55 +0300

    Change mobile menu

--- web_modules/Menu/index.js
+++ web_modules/Menu/index.js
@@ -1,22 +1,43 @@
 import { slide as Menu } from "react-burger-menu"
 import { Link } from "react-router"
+import { Link as ScrollLink } from "react-scroll"
 import Radium from "radium"
 import "./index.global.css"
 import links from "../Nav/links.json"
 
 const RadiumLink = Radium(Link)
+const RadiumScrollLink = Radium(ScrollLink)
 
 export default () => (
   <Menu>
     <ul className={ 'bm-list' }>
       {
-        links.map(({ title, url }, index) => (
-          <li key={ index } className={ 'bm-list__item' }>
-            <RadiumLink className={ 'bm-list__link' } to={ url }>
-              { title }
-            </RadiumLink>
-          </li>
-        ))
+        links.map(({ title, url }, index) => {
+          const props = {
+            className: "bm-list__link",
+            to: url,
+            ...index && {
+              smooth: true,
+              duration: 2000,
+            },
+          }
+
+          if (!index) return (
+            <li key={ index } className={ 'bm-list__item' }>
+              <RadiumLink { ...props }>
+                { title }
+              </RadiumLink>
+            </li>
+          )
+
+          return (
+            <li key={ index } className={ 'bm-list__item' }>
+              <RadiumScrollLink { ...props }>
+                { title }
+              </RadiumScrollLink>
+            </li>
+          )
+        })
       }
     </ul>
   </Menu>
