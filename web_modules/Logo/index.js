696efa87df1f838b3ff1c3b506de9c4fca017532
SergKazakov <sergeykazakoff@yandex.ru>
2016-08-08 10:42:47 +0300

    Convert logo to base64

--- web_modules/Logo/index.js
+++ web_modules/Logo/index.js
@@ -3,7 +3,7 @@ import styles from "./index.css"
 
 const Logo = () => (
   <Link className={ styles.rapper } to={ "/" }>
-    <img className={ styles.logo } src={ require("!!file!./logo.png") } />
+    <img className={ styles.logo } src={ require("./logo.png") } />
   </Link>
 )
 
